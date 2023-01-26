56bee537bf669ddad78d25bdc43a63832e483f75
VadimBerezhnoi <tsigma66@gmail.com>
2023-01-27 00:34:36 +0300

    Написал программу на вывод максимально числа из трех введенных

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+Console.Clear();
+
+Console.WriteLine("Введите число A ");
+Console.WriteLine("Введите число B ");
+Console.WriteLine("Введите число C ");
+
+int numA = int.Parse(Console.ReadLine()!);
+int numB = int.Parse(Console.ReadLine()!);
+int numC = int.Parse(Console.ReadLine()!);
+
+if (numA > numB && numA > numC)
+{
+  Console.WriteLine($"{numA} максимальное число");
+}
+
+else if (numB > numA && numB > numC)
+{
+    Console.WriteLine($"{numB} максимальное число");
+}
+
+else if (numC > numA && numC > numB)
+{
+    Console.WriteLine($"{numC} максимальное число");
+}
